3b2506a91ff83ab99231ad66e847c92d86fd684a
Kate Sliney <ksliney@gmail.com>
2020-03-07 18:25:29 -0500

    made d3 charts and leaflet map

--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -3,14 +3,16 @@
 
 'use strict';
 
-import 'core-js';
-import 'regenerator-runtime/runtime';
-import $ from 'jquery';
-import { Link } from '../_modules/link/link';
+import "core-js";
+import "regenerator-runtime/runtime";
+import $ from "jquery";
+import { Link } from "../_modules/link/link";
 import "./_charts";
+import "./_map";
 
 $(() => {
   new Link(); // Activate Link modules logic
   console.log('Welcome to Yeogurt!');
 });
+
 
